a1f9331b73f7053b70b93bc00f5cf855b5258459
Colin Lienard <colin.lienard87@gmail.com>
2025-11-25 20:47:59 +0100

    feat: add loading indicator

--- app/app.vue
+++ app/app.vue
@@ -7,6 +7,7 @@
 				content="Explore the world's top 100 most starred GitHub repositories, from frameworks to languages to learning resources, with a highlight section."
 			/>
 		</Head>
+		<NuxtLoadingIndicator color="oklch(77.7% 0.152 181.912)" :height="4" :duration="4000" />
 		<Layout>
 			<NuxtPage />
 		</Layout>
